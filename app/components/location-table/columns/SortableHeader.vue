9d068914025000a3ab38aef4303481377c16e6dd
Kay <amerk86@gmail.com>
2025-08-24 01:35:01 +0200

    styling: header sorting button

--- app/components/location-table/columns/SortableHeader.vue
+++ app/components/location-table/columns/SortableHeader.vue
@@ -16,7 +16,7 @@ const emit = defineEmits<Emits>();
 </script>
 
 <template>
-  <Button variant="ghost" @click="emit('toggle-sort')">
+  <Button variant="ghost" @click="emit('toggle-sort')" class="-ml-2 !px-2">
     {{ title }}
     <ArrowUp v-if="props.isSorted === 'asc'" class="ml-2 h-4 w-4" />
     <ArrowDown v-else-if="props.isSorted === 'desc'" class="ml-2 h-4 w-4" />
